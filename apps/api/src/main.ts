b4130f136d4962a4297f80d384182b205ac6da40
Antonio Reid <read.musik@gmail.com>
2024-03-21 22:29:01 -0500

    ts-config fixes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-try {
-  bootstrap();
-} catch (e) {
-  console.error(e);
-}
+
+bootstrap().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
