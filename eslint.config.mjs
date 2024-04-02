c2098b090e115ca9584d716e77ed6cfa938514e5
Antonio Reid <read.musik@gmail.com>
2024-04-01 22:17:14 -0500

    eslint fixes to configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,37 @@
 //  @ts-check
 
 import eslint from "@eslint/js"
+import prettierConfig from "eslint-config-prettier"
+import jestPlugin from "eslint-plugin-jest"
 import tseslint from "typescript-eslint"
 
 export default tseslint.config(
+  {
+    ignores: ["**/dist/**"],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
+  prettierConfig,
+  {
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      jest: jestPlugin,
+    },
+  },
   {
     languageOptions: {
+      parser: tseslint.parser,
       parserOptions: {
         project: true,
-        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
-    files: ["*.js", "*.cjs"],
+    files: ["**/*.js", "**/*.mjs"],
     ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    files: ["**/test/**"],
+    ...jestPlugin.configs["flat/recommended"],
   },
 )
